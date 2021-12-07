bee585715734cd357de0e7d77be25f797e225108
Georg Popp <Georg.Popp@outlook.com>
2021-12-07 14:06:55 +0100

    Initial setup
    create angular project
    add angular/material
    add api-key to environment (dev & prod)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://api.openweathermap.org/data/2.5/',
+  apiKey: '040bf863b4b8217104187907a74402d6',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://api.openweathermap.org/data/2.5/',
+  apiKey: '040bf863b4b8217104187907a74402d6',
 };
 
 /*
